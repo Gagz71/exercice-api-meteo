2d32efe6a96345b2cfb74a5fcd164c07a54bee00
Douns <sangodoun@protonmail.com>
2020-04-29 13:17:50 +0200

    exercice meteo fonctionnelle

--- index.php
+++ index.php
@@ -9,6 +9,24 @@
 <body>
 
 
+    <h1>EXERCICE METEO</h1>
+
+    <button>Voir la météo sur ma position actuelle</button>
+
+    
+
+    
+
+    <div class="view" id="view">
+        
+    </div>
+
+    <div class="previsions">
+        <div class="jeudi"></div>
+        <div class="vendredi"></div>
+        <div class="samedi"></div>
+        <div class="dimanche"></div>
+    </div>
 
 
 
@@ -18,13 +36,13 @@
 
 
 
-
-
-
-
-
-
-
+    <footer>
+        <p>Lien pour récupérer les données météo d'une position via coordonnées GPS (longitude et lattitude):
+            <a href="https://www.prevision-meteo.ch/services/json/">https://www.prevision-meteo.ch/services/json/lat=XXXlng=YYY</a>
+            (remplacer XXX par la latitude et YYY par la longitude)</p>
+        <p>pdf explicatif de l'api: 
+        <a href="https://prevision-meteo.ch/uploads/pdf/recuperation-donnees-meteo.pdf">https://prevision-meteo.ch/uploads/pdf/recuperation-donnees-meteo.pdf</a></p>
+    </footer>
 
     <script src="https://code.jquery.com/jquery-3.5.0.min.js"></script>
     <script src="js/script.js"></script>
---
+++ js/script.js
@@ -0,0 +1,149 @@
+//Mon code qui marche mais beaucoup de répétitions qui pourrait être factoré 
+//Voir la correction d'Anthony
+
+
+//Création d'une fonction qui appliquera mon overlay avec l'ajax loader
+function setOverlay(){
+    $('body').append('<div class="overlay"><img src="img/ajax-loader.gif"></div>');
+}
+
+//Création d'une fonction permettant de supprimer l'overlay
+function removeOverlay(){
+    $('.overlay').remove();
+}
+
+//Création d'une fonction qui affichera message d'erreur
+function displayErrorMessage(text){
+    $('#view').html('<p class="error">' + text + '</p>');
+}
+
+//Fonction équivalente a htmlspecialchars mais en js
+function escapeHtml(text) {
+
+    text = text.toString(); //On rajoute cette ligne qui permet la conversion de chiffre en string car la fonction n'est valable de base que pr remplacer les chaines de texte, et nous en dessous on a des chiffres a afficher (humidité, température etc ..)
+
+    var map = {
+      '&': '&amp;',
+      '<': '&lt;',
+      '>': '&gt;',
+      '"': '&quot;',
+      "'": '&#039;'
+    };
+    return text.replace(/[&<>"']/g, function(m) { return map[m]; });
+}
+
+// Si le bouton est cliqué
+$('button').click(function(){
+
+    // Paramétrages de la geolocalisation
+    let options = {
+        enableHighAccuracy: true,       // Activation de la haute précision
+        timeout: 5000,                  // Temps en ms avant timeout
+        maximumAge: 0                   // Desactive le cache gps
+    }
+
+   
+    // Fonction appelé en cas de pb lors de la récup des coordonnées GPS
+    let error = function(e){
+        if(e.code == e.TIMEOUT){
+            displayErrorMessage('Temps expiré');
+        } else if(e.code == e.PERMISSION_DENIED){
+            displayErrorMessage('Vous avez refusé la géolocalisation');
+        } else if(e.code == e.POSITION_UNAVAILABLE){
+            displayErrorMessage('Localisation impossible');
+        } else {
+            displayErrorMessage('Problème inconnu');
+        }
+    }
+
+    // Fonction  appelée en cas de réussite de récup des coordonnées GPS
+    let success = function(p){
+
+        //Enregistrement des coordonnées GPS récupéré dans variables
+        let latitude = p.coords.latitude;
+        let longitude = p.coords.longitude;
+
+        //Ca c'est pour moi 
+        console.log('Votre latitude actuelle est ' + latitude + ' et votre longitude est ' + longitude);
+
+        //Requête AJAX sur l'api
+        $.ajax({
+            type: 'GET',
+            url: 'https://www.prevision-meteo.ch/services/json/lat='+latitude+'lng='+longitude,
+            dataType : 'json',
+            timeout: 10000, //erreur de requête si au bout de 10 scdes ya rien 
+            success: function(data){ //Code lu en cas de succès
+
+                //ça aussi c'est pour moi 
+                console.log(data);
+                    
+                   
+
+                $('#view').html('');
+
+                $('#view').append(`
+                <h2>Météo actuelle sur votre postion</h2>
+                <p>`+ escapeHtml(data.current_condition.condition) +` <img src="` + escapeHtml(data.current_condition.icon) + ` "> </p>
+                <p>Lever du soleil : `+ escapeHtml(data.city_info.sunrise) +` / Coucher du soleil : ` + escapeHtml(data.city_info.sunset) + `</p>
+                <p>Température : ` + escapeHtml(data.current_condition.tmp) + ` degrés </p>
+                <p>Humidité : `+ escapeHtml(data.current_condition.humidity) + ` %</p>
+                <p>Vent : `+ escapeHtml(data.current_condition.wnd_spd) + ` km/h direction ` + escapeHtml(data.current_condition.wnd_dir) + `</p>
+                <p>Pression barométrique : ` + escapeHtml(data.current_condition.pressure) + ` hPa</p>
+                `);
+
+                $('.jeudi').html('');
+
+                $('.jeudi').append(`
+                <h3>Jeudi (30.04.2020)</h3>
+                <p>` + data.fcst_day_0.condition + ` <img src="` + escapeHtml(data.fcst_day_0.icon) + ` "  > </p>
+                <p>Température : de ` + data.fcst_day_0.tmin + ` degrés à ` + data.fcst_day_0.tmax + ` degrés</p>
+                `);
+                
+                $('.vendredi').html('');
+
+                $('.vendredi').append(`
+                <h3>Vendredi (01.05.2020)</h3>
+                <p>` + data.fcst_day_1.condition + ` <img src="` + escapeHtml(data.fcst_day_1.icon) + ` "  > </p>
+                <p>Température : de ` + data.fcst_day_1.tmin + ` degrés à ` + data.fcst_day_1.tmax + ` degrés</p>
+                `);
+
+                $('.samedi').html('');
+
+                $('.samedi').append(`
+                <h3>Samedi (02.05.2020)</h3>
+                <p>` + data.fcst_day_2.condition + ` <img src="` + escapeHtml(data.fcst_day_2.icon) + ` " ></p>
+                <p>Température : de ` + data.fcst_day_2.tmin + ` degrés à ` + data.fcst_day_2.tmax + ` degrés</p>
+                `);
+
+                $('.dimanche').html('');
+
+                $('.dimanche').append(`
+                <h3>Dimanche (03.05.2020)</h3>
+                <p>` + data.fcst_day_3.condition + ` <img src="` + escapeHtml(data.fcst_day_3.icon) + ` "  > </p>
+                <p>Température : de ` + data.fcst_day_3.tmin + ` degrés à ` + data.fcst_day_3.tmax + ` degrés</p>
+                `);
+
+
+
+            }, error: function(){
+                displayErrorMessage('Problème lors de la récupération des données météo');
+            },
+            //Avant la requête AJAX
+            beforeSend: function(){
+                //Mise en place de l'overlay
+                setOverlay();
+            }, 
+            //Au retour de la requête AJAX
+            complete: function(){
+                //Suppression de l'overlay
+                removeOverlay();
+            }
+        });
+
+    }
+
+    //Récupération des coordonnées de l'utilisateur
+    // Code permettant de mettre en place la demande de geolocalisation au navigateur
+    navigator.geolocation.getCurrentPosition(success, error, options);
+
+});
